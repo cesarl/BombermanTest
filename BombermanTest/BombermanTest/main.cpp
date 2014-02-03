025d4b92e9ec0bc2faab3735866a87fdc137d268
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-03 15:41:02 +0100

    Test with textures

--- BombermanTest/BombermanTest/main.cpp
+++ BombermanTest/BombermanTest/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include "GameEngine.hpp"
+#include <Core/Texture.hh>
 
 unsigned int main()
 {
@@ -7,6 +8,10 @@ unsigned int main()
 
 	if (!engine.initialize())
 		return EXIT_FAILURE;
+
+	Texture test;
+	test.load("./Assets/SunTexture.tga");
+
 	while (engine.update())
 	{
 		engine.draw();
