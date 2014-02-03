84d64559903b82add4a8296e84adc24aae37b8f5
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-03 18:27:18 +0100

    Some tests

--- BombermanTest/BombermanTest/main.cpp
+++ BombermanTest/BombermanTest/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include "GameEngine.hpp"
 #include <Core/Texture.hh>
+#include <Core/AShader.hh>
+#include <Model.hh>
 
 unsigned int main()
 {
@@ -11,6 +13,14 @@ unsigned int main()
 
 	Texture test;
 	test.load("./Assets/SunTexture.tga");
+
+	AShader shader;
+	auto res = shader.load("./Shaders/test.fragment", GL_FRAGMENT_SHADER);
+	res = shader.load("./Shaders/test.vertex", GL_VERTEX_SHADER);
+	res = shader.build();
+
+	gdl::Model model;
+	model.loadFromFbx("./Assets/Lenny The Sanake.FBX");
 
 	while (engine.update())
 	{
