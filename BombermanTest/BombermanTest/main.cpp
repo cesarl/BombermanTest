d43e2e7d817cf118dab6f0a3008c18e213f326be
Cesar Leblic <cesar.leblic@gmail.com>
2014-02-03 12:48:39 +0100

    Basic loop

--- BombermanTest/BombermanTest/main.cpp
+++ BombermanTest/BombermanTest/main.cpp
@@ -1,6 +1,15 @@
 #include <cstdlib>
+#include "GameEngine.hpp"
 
 unsigned int main()
 {
+	GameEngine engine;
+
+	if (!engine.initialize())
+		return EXIT_FAILURE;
+	while (engine.update())
+	{
+		engine.draw();
+	}
 	return EXIT_SUCCESS;
 }
